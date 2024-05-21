b98b57f82de453651c40a3152fbde53b3426811e
HamedPoor84 <hamedpoor84@gmail.com>
2024-05-21 18:49:57 +0330

    complete semaphore

--- src/main/java/sbu/cs/Semaphore/Controller.java
+++ src/main/java/sbu/cs/Semaphore/Controller.java
@@ -1,5 +1,7 @@
 package sbu.cs.Semaphore;
 
+import java.util.concurrent.Semaphore;
+
 public class Controller {
 
     /**
@@ -19,11 +21,12 @@ public class Controller {
      */
 
     public static void main(String[] args) {
-        Operator operator1 = new Operator("operator1");
-        Operator operator2 = new Operator("operator2");
-        Operator operator3 = new Operator("operator3");
-        Operator operator4 = new Operator("operator4");
-        Operator operator5 = new Operator("operator5");
+        Semaphore semaphore = new Semaphore(2 );
+        Operator operator1 = new Operator("operator1" , semaphore);
+        Operator operator2 = new Operator("operator2" , semaphore);
+        Operator operator3 = new Operator("operator3" , semaphore);
+        Operator operator4 = new Operator("operator4" , semaphore);
+        Operator operator5 = new Operator("operator5" , semaphore);
 
         operator1.start();
         operator2.start();
@@ -31,12 +34,7 @@ public class Controller {
         operator4.start();
         operator5.start();
 
-        /*
-        TODO
-            Use a Semaphore to solve the synchronization problem.
-            Every time a thread accesses the resource, print its Name and the current System Time.
-            Combine the 4 resulting blocks to create the final matrix product and return it.
-         */
+
 
     }
 }
